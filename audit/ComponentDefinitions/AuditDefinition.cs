d92b1247f86f4a43eefebe9217d4eb2d0704184f
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2021-10-26 23:15:00 +0300

    added page for viewing models

--- audit/ComponentDefinitions/AuditDefinition.cs
+++ audit/ComponentDefinitions/AuditDefinition.cs
@@ -22,19 +22,14 @@ public class AuditDefinition : IComponentDefinition
 
     public void DefineComponents(WebApplication app)
     {
-        app.MapGet("/hello-world", () =>
-            {
-                return "hello world!";
-            });
-            app.MapPost("/audit", async (AuditModel model, AuditService service)=>{
-                //TODO: adjsut performance 
-                await service.SaveAuditModel(model);
-            
-                ///Console.WriteLine(model.Body);
-            });
+        app.MapPost("/audit", async (AuditModel model, AuditService service)=>{
+            //TODO: adjsut performance 
+            await service.SaveAuditModel(model);
+        
+            ///Console.WriteLine(model.Body);
+        });
 
-            app.MapGet("/audit/{id}", Task<AuditModel> (Guid id, AuditService service) => service.GetAuditModel(id));
-            app.MapGet("/audit/{id}/models", Task<IEnumerable<AuditModel>> (Guid id, AuditService service) => service.GetModelsOfAuditableObjectThroughItsLifecycle(id));
-             
+        app.MapGet("/audit/{id}", Task<AuditModel> (Guid id, AuditService service) => service.GetAuditModel(id));
+        app.MapGet("/audit/{id}/models", Task<IEnumerable<AuditModel>> (Guid id, AuditService service) => service.GetModelsOfAuditableObjectThroughItsLifecycle(id));        
     }
 }
