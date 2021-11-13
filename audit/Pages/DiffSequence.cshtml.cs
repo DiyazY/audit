c98eec69657f185c5153ed056139bb3de5a03a05
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2021-11-13 22:38:19 +0200

    cleaned up usings

--- audit/Pages/DiffSequence.cshtml.cs
+++ audit/Pages/DiffSequence.cshtml.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using audit.Models;
 using audit.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace;
--- audit/Pages/Sequence.cshtml.cs
+++ audit/Pages/Sequence.cshtml.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using audit.Models;
 using audit.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
