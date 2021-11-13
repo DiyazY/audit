16d89d279b5af3946a78a46dbeb514f0b18ba35b
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2021-11-13 22:29:22 +0200

    fixed all errors due to updating to .NET6 release

--- audit/Program.cs
+++ audit/Program.cs
@@ -1,4 +1,7 @@
 using audit.Utils;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
