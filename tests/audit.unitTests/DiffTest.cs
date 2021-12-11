1fed64bc613d1d7547c9b28495b4694d3128e9f6
Diyaz Yakubov <diyaz.yakubov@wunderdog.fi>
2021-12-11 23:33:43 +0200

    added verify lib, and one more unit test

--- tests/audit.unitTests/DiffTest.cs
+++ tests/audit.unitTests/DiffTest.cs
@@ -1,21 +1,40 @@
 using audit.Utils;
+using System.Threading.Tasks;
+using VerifyXunit;
 using Xunit;
 
 namespace audit.unitTests;
 
+[UsesVerify]
 public class DiffTest
 {
     [Fact]
-    public void Test1()
+    public Task Diff_Get_Should_Return_Right_JsonDiff()
     {
         // arrange
-        var a = "{\"name\": \"diyaz\"}";
-        var b = "{\"name\": \"diyaz yakubov\"}";
+        var oldObject = "{\"name\": \"diyaz\"}";
+        var newObject = "{\"name\": \"diyaz yakubov\"}";
         
         // act
-        var diff = Diff.Get(a, b);
+        var diff = Diff.Get(oldObject, newObject);
 
         // assert
         Assert.NotNull(diff);
+        return Verifier.VerifyJson(diff);
+    }
+
+    [Fact]
+    public Task Diff_Unpatch_Should_Return_OlderObject_By_CurrentObject_and_JsonDiff()
+    {
+        // arrange
+        var diff = "{\"name\": [\"diyaz\",\"diyaz yakubov\"]}";
+        var currentObject = "{\"name\": \"diyaz yakubov\"}";
+        
+        // act
+        var olderObject = Diff.Unpatch(currentObject, diff);
+
+        // assert
+        Assert.NotNull(olderObject);
+        return Verifier.VerifyJson(olderObject);
     }
 }
